eb45f4aa7a1ee96780f13000f0d823f23721982b
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2022-12-30 07:01:02 +0500

    p

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('welcome');
 let songIndex = 0;
-let audioElement = new Audio ('1.mp3');
+let audioElement = new Audio ('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif')
@@ -27,13 +27,13 @@ masterPlay.addEventListener('click', () =>{
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity= 1;
     }else{
-        audioElement.paused();
+        audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
         masterPlay.classList.add('fa-play-circle');    }
 })
 
-audioElement.addEventListener('timeUpdate' , ()=>{
-    console.log('timeUpdate')
+audioElement.addEventListener('timeupdate' , ()=>{
+    console.log('timeupdate')
     progress = parseInt(audioElement.currentTime/audioElement.duration)*100;
     myProgressBar.value = progress;
 })
